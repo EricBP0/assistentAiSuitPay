0be1346f7b898d12b4e4c16ecb87f4fb59425dac
EricBP0 <ericborgesp@gmail.com>
2024-04-19 17:05:35 -0300

    first commit

---
+++ testAssistent.py
@@ -0,0 +1,23 @@
+from openai import OpenAI
+import json
+
+client = OpenAI(api_key='')
+
+assistant = client.beta.assistants.retrieve("asst_k5znPftzQtXVBdLNC5SSTuF5")
+thread = client.beta.threads.create()
+message = client.beta.threads.messages.create(
+    thread_id=thread.id,
+    role="user",
+    content="Quais são os parâmetros que tenho que passar para gerar um qrCode pela api da SuitPay?"
+)
+run = client.beta.threads.runs.create_and_poll(
+    thread_id=thread.id,
+    assistant_id=assistant.id,
+)
+if run.status == 'completed':
+    messages = client.beta.threads.messages.list(
+        thread_id=thread.id
+    )
+    print(message)
+else:
+    print(run.status)
